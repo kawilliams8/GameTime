11dd78ee0912a9a18407b3060e89b98c12fe7699
Eric O'Neill <oneill.eric23@gmail.com>
2019-07-13 12:51:39 -0600

    Build player class and test file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
 
-// An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
 
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 import Game from './Game';
+import Turn from './Turn';
+import Player from './Player';
+import Round from './Round';
 
 let game = new Game()
 
-console.log('This is the JavaScript entry file - your code begins here.');
---
+++ test/Player-test.js
@@ -0,0 +1,22 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+import Player from '../src/Player.js';
+// import spies from 'chai-spies';
+// import DOMupdates from '../src/DOMupdates.js';
+// chai.use(spies);
+// import data from '../src/Data.js';
+
+// chai.spy.on(DOMupdates, 'updateLater', () => true);
+
+describe('Player', () => {
+	let player;
+	beforeEach(() => {
+		player = new Player('Djavan');
+	});
+
+	it('should be a function that instantiates a player', () => {
+		expect(Player).to.be.a('function');
+		expect(player).to.be.an.instanceof(Player)
+	})
+})
