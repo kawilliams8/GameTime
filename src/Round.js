e439839125178202328bd76c44fde683fccc750d
Djavan Munroe <djavanm24@gmail.com>
2019-07-13 15:07:34 -0600

    Add commit for git workflow

--- src/Round.js
+++ src/Round.js
@@ -1,3 +1,22 @@
+class Round {
+  constructor(playerOne, playerTwo, currentSurvey) {
+    this.playerOne = playerOne;
+    this.playerTwo = playerTwo;
+    this.currentSurvey = currentSurvey;
 
+  }
+}
+
+class FullRound extends Round {
+  constructor(playerOne, playerTwo, currentSurvey) {
+    super(playerOne, playerTwo, currentSurvey)
+    this.playerOne = playerOne;
+    this.playerTwo = playerTwo;
+    this.currentSurvey = currentSurvey;
+    this.turnCounter = 1;
+    this.currentPlayer;
+    
+  }
+}
 
 export default Round;
