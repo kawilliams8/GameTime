602e9ca446f9b740c03320a8ebb69c48bfb838f4
Djavan Munroe <djavanm24@gmail.com>
2019-07-15 15:15:03 -0600

    Add HTML first draft

--- src/index.js
+++ src/index.js
@@ -14,12 +14,9 @@ import Game from '../src/Game.js';
 
 let game = new Game(data)
 
-const player1, player2;
-let turn, round;
-
 function createPlayers() {
-  let playerOneName = $(//playerInput1).text();
-  let playerTwoName = $(//playerInput2).text();
+  let playerOneName = $(playerInput1).text();
+  let playerTwoName = $(playerInput2).text();
   player1 = new Player(playerOneName);
   player2 = new Player(playerTwoName);
   game.selectSurvey();
